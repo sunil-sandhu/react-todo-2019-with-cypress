823e1ee8fa731c39322bb0bd195c8d1fc4b827a5
Sunil Sandhu <sunil.sandhu87@gmail.com>
2019-10-25 17:36:07 +0200

    ready

--- cypress/integration/App.test.js
+++ cypress/integration/App.test.js
@@ -9,18 +9,15 @@ describe("<App/>", () => {
 
   describe("The default UI", () => {
     it("Renders two default todo items", () => {
-      // expect(app.find(".ToDoItem").length).toBe(2);
+      cy.get(".ToDoItem").should("have.length", 2);
     });
 
     it("Has an input field", () => {
-      cy.get("input");
-      // expect(app.find("input").length).toEqual(1);
+      cy.get("input").should("have.length", 1);
     });
 
     it("Has an add button", () => {
-      cy.get(".ToDo-Add").contains("+");
-
-      // expect(app.find(".ToDo-Add").length).toEqual(1);
+      cy.get(".ToDo-Add").should("have.length", 1);
     });
   });
 
@@ -34,16 +31,16 @@ describe("<App/>", () => {
         .type("Create more tests")
         .should("have.value", "Create more tests");
       cy.get(".ToDo-Add").click();
-      cy.get(".ToDoItem-Text").contains("Create more tests");
+      cy.get(".ToDoItem").should("have.length", 3);
     });
   });
 
   describe("Deleting items", () => {
-    it("When the delete button is pressed for a todo item, it removes the entire item", () => {
+    it("When the delete button is pressed for the first todo item, it removes the entire item", () => {
       cy.get(":nth-child(1) > .ToDoItem-Delete").click();
     });
     it("means that because the first toDoItem was deleted, the first toDoItem should now be buy milk", () => {
-      cy.get(":nth-child(1)").contains("buy milk");
+      cy.get(":nth-child(2) > .ToDoItem").contains("buy milk");
     });
   });
 });
